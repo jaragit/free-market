d556a72bfd953e4acc31ccb4b511beb7aa15858a
jaragit <akihiro.fujihara@di-v.co.jp>
2020-08-05 16:25:57 +0900

    items_edit_destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item.images.build
     render layout: 'no_menu' # レイアウトファイル指定
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,12 +7,12 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: :true
   has_many :images, dependent: :destroy 
-  accepts_nested_attributes_for :images 
+  accepts_nested_attributes_for :images ,allow_destroy: true, update_only: true
 
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :images, length: { minimum: 1, maximum: 5, message: "の数が不正です" } 
-  
+
   enum condition:{
     "新品、未使用": 0,
     "未使用に近い": 1,
